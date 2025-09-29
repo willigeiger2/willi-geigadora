bebfa5d0ff3aa690f5f1f65fd27af70da48ed8ba
Willi <willi@cloudflare.com>
2025-09-29 22:33:43 +0100

    Added LinearField, used just to implement upward motion so far. Need to think about how to attach falloffs for cursor drag.

---
+++ src/scripts/LinearField.ts
@@ -0,0 +1,17 @@
+import { type Field, FieldOutput, Point } from "../scripts/ParticleSystem";
+export { LinearField };
+
+class LinearField implements Field {
+	public constructor(public direction: Point) {}
+
+	evaluate(points: Point[]) : FieldOutput {
+		let size = points.length;
+		let field = new Array<Point>(size);
+		let falloff = new Array<number>(size);
+		for (let i = 0; i < size; i++) {
+			field[i] = this.direction;
+			falloff[i] = 1.0;
+		}
+		return new FieldOutput(field, falloff);
+	}
+}
